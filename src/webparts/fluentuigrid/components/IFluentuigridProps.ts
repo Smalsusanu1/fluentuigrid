554725d01fc584e58f8a862689a6a2391dd18df4
Anubhav Shukla <anubhav@smalsus.com>
2022-11-11 15:53:40 +0530

    first commit

---
+++ src/webparts/fluentuigrid/components/IFluentuigridProps.ts
@@ -0,0 +1,7 @@
+export interface IFluentuigridProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/webparts/fluentuigrid/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface IFluentuigridWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'FluentuigridWebPartStrings' {
+  const strings: IFluentuigridWebPartStrings;
+  export = strings;
+}
